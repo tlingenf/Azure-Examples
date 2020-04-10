3ec4e6f00c7839d8fde312ff1976f4e2b3c6f109
Travis Lingenfelder <trlingen@microsoft.com>
2020-04-10 10:34:33 -0500

    checkin all

---
+++ Az.StorageAccount/Az.StorageAccount.PowerShell/Benchmark-storage-account.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module Az.Accounts
+Import-Module Az.Storage
+
+Connect-AzAccount
+
+$subscriptionId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+$storageResourceGroupName = "core.shared"
+$storageAccountName = "storagename"
+$storageContainerName = "loadtest"
+
+Select-AzSubscription -SubscriptionId $subscriptionId
+
+$storageAccount = Get-AzStorageAccount -Name $storageAccountName -ResourceGroupName $storageResourceGroupName
+
+$storageKeys = $storageAccount | Get-AzStorageAccountKey
+$destinationContext = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageKeys[0].Value
+
+$containerSASURI = New-AzStorageContainerSASToken -Context $destinationContext -ExpiryTime(get-date).AddSeconds(3600) -FullUri -Name $storageContainerName -Permission rwld
+
+azcopy bench $containerSASURI --size-per-file 4541K --file-count 1 --block-size-mb 0.0000886917114257812
